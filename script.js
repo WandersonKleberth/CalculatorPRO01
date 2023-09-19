90023646469c32f3302b209e314711aac4fee937
Wanderson Klebert <101153218+WandersonKleberth@users.noreply.github.com>
2023-09-19 16:27:11 -0300

    código enxuto
    fazendo um algoritmo um pouco mais enxuto

--- script.js
+++ script.js
@@ -1,24 +1,57 @@
-function insert(num) {
-    var numero = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = numero + num;
+// function insert(num) {
+//     var numero = document.getElementById('resultado').innerHTML;
+//     document.getElementById('resultado').innerHTML = numero + num;
+// }
+
+// function clean() {
+//     document.getElementById('resultado').innerHTML = "";
+// }
+
+// function back(){
+//     var resultado = document.getElementById('resultado').innerHTML;
+//     document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1)
+// }
+
+// function calcular (){
+//     var resultado = document.getElementById('resultado').innerHTML;
+//     if(resultado)
+//     {
+//         document.getElementById('resultado').innerHTML = eval(resultado);
+//     }
+//     else{
+//         document.getElementById('resultado').innerHTML = "Digite um valor...";
+//     }
+// }
+
+// função para adicionar os números e operações no campo de resultado da calculadora
+function insert(value) {
+    var resultado = document.getElementById("resultado");
+    resultado.innerText += value;
 }
 
+// função para limpar o campo de resultado da calculadora.
 function clean() {
-    document.getElementById('resultado').innerHTML = "";
+    var resultado = document.getElementById("resultado");
+    resultado.innerText = "";
 }
 
-function back(){
-    var resultado = document.getElementById('resultado').innerHTML;
-    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1)
+// função para apagar o último caractere do campo de resultado da calculadora.
+function back() {
+    var resultado = document.getElementById("resultado");
+    resultado.innerText = resultado.innerText.slice(0, -1);
 }
 
-function calcular (){
-    var resultado = document.getElementById('resultado').innerHTML;
-    if(resultado)
-    {
-        document.getElementById('resultado').innerHTML = eval(resultado);
+// função para realizar o cálculo da expressão matemática no campo de resultado da calculadora.
+function calcular() {
+    var resultado = document.getElementById("resultado");
+    var expressao = resultado.innerText;
+
+    if (expressao === "") {
+        alert("Por favor, digite uma expressão antes de calcular.");
+        return;
     }
-    else{
-        document.getElementById('resultado').innerHTML = "Digite um valor...";
-    }
+
+    const calcularResultado = new Function(`return ${expressao}`);
+    const resultadoFinal = calcularResultado();
+    resultado.innerText = resultadoFinal;
 }
